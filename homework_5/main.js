0ea57b2274e35cdcac09e51093137f6fea3b90fe
Nancy <nzzuo@andrew.cmu.edu>
2021-03-24 22:04:39 -0400

    added js file

---
+++ homework_5/main.js
@@ -0,0 +1,9 @@
+// from javascript shopping cart tutorial: https://www.youtube.com/watch?v=PoTGs38DR9E
+
+let carts = document.querySelectorAll(".add-cart");
+
+console.log("hi")
+
+for (let i=0; i < carts.length; i++) {
+    console.log("my loop");
+}
